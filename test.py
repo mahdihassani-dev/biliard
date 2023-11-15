109829d5d270aa93cf48d261f44d8ed32b937ba1
Mahdi <m.hassani4951383@gmail.com>
2023-11-15 14:50:33 +0330

    base of project

--- test.py
+++ test.py
@@ -1,20 +1,19 @@
-import numpy as np
+
 import matplotlib.pyplot as plt
 
-plt.axis([0, 2, 0, 1])
-plt.xticks([1, 2])
-plt.yticks([1])
-
-velocity = 0
-pos = 1.5
-for i in range(10, 0, -1):
-    scat = plt.scatter(pos - velocity, 0.5, c='black')
-    plt.pause(1)
-    if i != 1:
-        scat.remove()
-    pos = pos - velocity
-    if i == 10:
-        velocity = 1
-    velocity *= 0.5
-
+point = plt.plot(0, 0, "o")[0]
+plt.ylim(0, 5)
+plt.xlim(0, 5)
+plt.ion()
 plt.show()
+
+point.set_data(4, 2.5)
+plt.pause(1)
+
+a = [[3.9, 2.5], (3.8, 2.5), (3.7, 2.5), (3.6, 2.5), (3.5, 2.5), (3.4, 2.5), (3.3, 2.5), (3.2, 2.5), (3.1, 2.5), (3.0, 2.5)]
+
+for i in range(len(a)):
+     point.set_data(a[i][0], a[i][1])
+     plt.pause(0.01)
+
+plt.show(block=True)
