9de50fa9c596f64120634fab2c742eda78da1659
Mahdi <m.hassani4951383@gmail.com>
2023-11-16 20:25:43 +0330

    speed to 0.1

--- biliard.py
+++ biliard.py
@@ -18,7 +18,7 @@ a = [[3.9, 2.5], (3.8, 2.5), (3.7, 2.5), (3.6, 2.5), (3.5, 2.5), (3.4, 2.5), (3.
 
 for i in range(len(a)):
      point.set_data(a[i][0], a[i][1])
-     plt.pause(0.01)
+     plt.pause(0.1)
 
      if (a[i][0] == 0 or a[i][0] == 5) and (a[i][1] == 0 or a[i][1] == 5):
           print("You won!")
