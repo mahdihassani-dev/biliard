d1d48e363f1d70e3918ad2dc0e248b240887ceb6
Mahdi <m.hassani4951383@gmail.com>
2023-11-16 21:04:26 +0330

    clean and comments

--- biliard.py
+++ biliard.py
@@ -1,28 +1,44 @@
-
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
+# game table setup
 fig, ax = plt.subplots()
 board = patches.Rectangle((0, 0), 5, 5, facecolor='green')
 ax.add_patch(board)
 point = plt.plot(0, 0, "o", color='black')[0]
-#plt.ylim(0, 5)
-#plt.xlim(0, 5)
+point.set_data(4, 2.5)
+
+# show table with anim
 plt.ion()
 plt.show()
 
-point.set_data(4, 2.5)
-plt.pause(1)
+# game variables
+ballIsMoving = False
 
-a = [[3.9, 2.5], (3.8, 2.5), (3.7, 2.5), (3.6, 2.5), (3.5, 2.5), (3.4, 2.5), (3.3, 2.5), (3.2, 2.5), (3.1, 2.5), (3.0, 2.5)]
 
-for i in range(len(a)):
-     point.set_data(a[i][0], a[i][1])
-     plt.pause(0.1)
+def isWinner(x, y):
+    if (x == 0 or x == 5) and (y == 0 or y == 5):
+        print("You won!")
+        return True
 
-     if (a[i][0] == 0 or a[i][0] == 5) and (a[i][1] == 0 or a[i][1] == 5):
-          print("You won!")
-          plt.pause(2)
-          break
 
-plt.show(block=True)
+# test values -------> they will be removed after implementing main values and functions
+a = [[3.9, 2.5], (3.8, 2.5), (3.7, 2.5), (3.6, 2.5), (3.5, 2.5), (3.4, 2.5), (3.3, 2.5), (3.2, 2.5), (3.1, 2.5),
+     (3.0, 2.5)]
+
+# game loop
+while True:
+
+    # movement of ball based on inputs
+    if ballIsMoving:
+        for i in range(len(a)):
+            point.set_data(a[i][0], a[i][1])
+            plt.pause(0.1)
+            if isWinner(a[i][0], a[i][1]):
+                break
+        ballIsMoving = False
+
+    # get velocity and angle from user
+    else:
+        input("velocity :")
+        ballIsMoving = True
