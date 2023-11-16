c92b04e3e6565e73df3168bdb32f9bb2a31b7f7c
tala <13830725amir@gmail.com>
2023-11-16 20:13:01 +0330

    win&color

--- biliard.py
+++ biliard.py
@@ -1,9 +1,13 @@
 
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
-point = plt.plot(0, 0, "o")[0]
-plt.ylim(0, 5)
-plt.xlim(0, 5)
+fig, ax = plt.subplots()
+board = patches.Rectangle((0, 0), 5, 5, facecolor='green')
+ax.add_patch(board)
+point = plt.plot(0, 0, "o", color='black')[0]
+#plt.ylim(0, 5)
+#plt.xlim(0, 5)
 plt.ion()
 plt.show()
 
@@ -15,5 +19,10 @@ a = [[3.9, 2.5], (3.8, 2.5), (3.7, 2.5), (3.6, 2.5), (3.5, 2.5), (3.4, 2.5), (3.
 for i in range(len(a)):
      point.set_data(a[i][0], a[i][1])
      plt.pause(0.01)
+
+     if (a[i][0] == 0 or a[i][0] == 5) and (a[i][1] == 0 or a[i][1] == 5):
+          print("You won!")
+          plt.pause(2)
+          break
 
 plt.show(block=True)
