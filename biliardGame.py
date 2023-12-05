354a3b3e7a75e73e52eb0736b0cbc049ce5d5b99
amirhosseintl04 <13830725amir@gmail.com>
2023-12-05 22:17:12 +0330

    Comments

--- biliardGame.py
+++ biliardGame.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 import math
 
+#Prerequisites for velocity calculation
 mass = 0.1
 alpha = 1
 g = 10
 t = 0.08
 fraction_k = 0.3
 
-
+# Calculates and updates velocity
 def getVelocity(par, v0, a, t, alpha, mass):
     match par:
         case True:
@@ -29,12 +30,12 @@ def updateVelocity(v0):
 
 
 def rectangle():
+    # Initialize the tkinter window
     base = tk.Tk()
     base.title("board")
     canvas = tk.Canvas(base, width=820, height=520)
     canvas.pack()
 
-    # Constants
 
     def aroundTheBorder(newA, a1, a2):
         if newA >= a2:
@@ -46,6 +47,9 @@ def rectangle():
     def drawO():
         nonlocal x, y, angle, x1, y1, x2, y2, velocity
         canvas.delete("o")
+
+    #Calculates the position of "O" and angles
+        #in the board
         newX = x - velocity * math.cos(math.radians(angle))
         newY = y - velocity * math.sin(math.radians(angle))
 
@@ -57,6 +61,7 @@ def rectangle():
             velocity = updateVelocity(velocity)
 
         else:
+            #out of board
             if not x1 < newX < x2:
                 aroundTheBorder(newX, x1, x2)
                 angle = 180 - angle
@@ -67,23 +72,24 @@ def rectangle():
 
             # velocity = getVelocity(True, velocity, a, t, alpha, mass)
             velocity = updateVelocity(velocity)
-
-        print(f'velocity : {velocity}')
+        #print(f'velocity : {velocity}')
 
         canvas.create_text(x, y, text="o", fill="black", font=("Arial", 20), tags="o")
 
+        #Positions of winning holes
         if ((x - 35) * (x - 35) + (y - 35) * (y - 35) < 225 or
                 (x - 785) * (x - 785) + (y - 35) * (y - 35) < 225 or
                 (x - 35) * (x - 35) + (y - 485) * (y - 485) < 225 or
                 (x - 785) * (x - 785) + (y - 485) * (y - 485) < 225):
-            victory()
+            print("YOU WON! ")
+            #victory()
         elif velocity > 0:
             base.after(80, drawO)
         else:
-            print(f'x = {x}')
-            print(f'y = {y}')
+            #print(f'x = {x}')
+            #print(f'y = {y}')
             newInputs()
-
+    #Gets new inputs after zero velocity
     def newInputs():
         nonlocal velocity, angle
         velocity = int(input("enter velocity : "))
@@ -91,12 +97,13 @@ def rectangle():
         newLine()
         drawO()
 
-    def victory():
-        Winwindow = tk.Toplevel()
-        Winwindow.title("victory")
-        label = tk.Label(Winwindow, text="You won!", font=("Arial", 16))
-        label.pack()
+    # def victory():
+    #     Winwindow = tk.Toplevel()
+    #     Winwindow.title("victory")
+    #     label = tk.Label(Winwindow, text="You won!", font=("Arial", 16))
+    #     label.pack()
 
+    #Draw new line
     def newLine():
         nonlocal angle, line
         canvas.delete("line")
@@ -106,15 +113,17 @@ def rectangle():
         yLineEnd = y + lineLength * math.sin(angleRad)
         line = canvas.create_line(x, y, xLineEnd, yLineEnd, fill="red", tags="line")
 
+    # Initialize the board
     x1, y1 = 20, 20
     x2, y2 = 800, 500
     canvas.create_rectangle(x1, y1, x2, y2, fill="green")
 
+    #first position of "o"
     x = 410
     y = 260
-
     canvas.create_text(x, y, text="o", fill="black", font=("Arial", 20), tags="o")
 
+    #winning holes
     x1hole1, y1hole1 = 20, 20
     x2hole1, y2hole1 = 50, 50
     canvas.create_oval(x1hole1, y1hole1, x2hole1, y2hole1, fill="black")
@@ -131,16 +140,16 @@ def rectangle():
     x2hole4, y2hole4 = 770, 470
     canvas.create_oval(x1hole4, y1hole4, x2hole4, y2hole4, fill="black")
 
+    #first inputs
     velocity = int(input("enter first velocity : "))
     angle = int(input("enter first angle : "))
-
     angleRad = math.radians(angle)
     lineLength = 300
     xLineEnd = x + lineLength * math.cos(angleRad)
     yLineEnd = y + lineLength * math.sin(angleRad)
-
-    canvas.create_text(x, y, text="o", fill="black", font=("Arial", 20), tags="o")
     line = canvas.create_line(x, y, xLineEnd, yLineEnd, fill="red", tags="line")
+
+    #waits for 0.08 sec
     base.after(80, drawO)
 
     base.mainloop()
